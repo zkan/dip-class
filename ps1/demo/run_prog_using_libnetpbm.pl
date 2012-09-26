23a836aedd38779b0b3435ad152fb98b3dad1001
Kan O <zkan.cs@gmail.com>
2012-09-26 20:02:21 +0700

    add/update demo and tests

--- ps1/demo/run_prog_using_libnetpbm.pl
+++ ps1/demo/run_prog_using_libnetpbm.pl
@@ -4,7 +4,7 @@
 # example ./run.pl (need to run under the demo folder)
 #
 
-my $prog = "prog";
+my $prog = "prog_using_libnetpbm";
 
 my $dir_code = "../code/";
 my $dir_input = "../data/input/";
@@ -14,9 +14,9 @@ my $input_image1 = $dir_input . "cat.pgm";
 my $input_image2 = $dir_input . "cat_lick.pgm";
 my $input_image3 = $dir_input . "sea_lion.pgm";
 
-my $output_image1 = $dir_output . "cat.pgm";
-my $output_image2 = $dir_output . "cat_lick.pgm";
-my $output_image3 = $dir_output . "sea_lion.pgm";
+my $output_image1 = $dir_output . "cat_by_libnetpbm.pgm";
+my $output_image2 = $dir_output . "cat_lick_by_libnetpbm.pgm";
+my $output_image3 = $dir_output . "sea_lion_by_libnetpbm.pgm";
 
 system("rm $output_image1");
 system("rm $output_image2");
---
+++ ps1/tests/run_prog_using_libnetpbm.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+#
+# run the C program
+# example: ./run.pl (need to run under the tests folder)
+#
+
+my $prog = "pgm_image_test";
+
+my $dir_code = "../code/";
+my $dir_output = "../data/output/";
+ 
+my $output_image1 = $dir_output . "cat_by_libnetpbm.pgm";
+my $output_image2 = $dir_output . "cat_lick_by_libnetpbm.pgm";
+my $output_image3 = $dir_output . "sea_lion_by_libnetpbm.pgm";
+
+print "Test if the output image is actullay in PGM format or not.\n";
+system($dir_code . $prog . " $output_image1");
+system($dir_code . $prog . " $output_image2");
+system($dir_code . $prog . " $output_image3");
+
